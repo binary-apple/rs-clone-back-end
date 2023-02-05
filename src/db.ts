739ade3e39d79963fcabb4b6aba9d46c7ce0a97f
Anna Perevedentseva <anna.pereved@gmail.com>
2023-02-05 13:55:41 +0100

    feat: organize project structure & add skeleton for user request

--- src/db.ts
+++ src/db.ts
@@ -2,5 +2,5 @@ import { initializeApp } from 'firebase/app';
 import { config } from './config';
 import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
 
-const app = initializeApp(config.firebaseConfig);
-export const db = getFirestore(app);
+// const app = initializeApp(config.firebaseConfig);
+export const db = getFirestore(initializeApp(config.firebaseConfig));
--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,17 @@ import express from 'express';
 import cors from 'cors';
 import * as bodyParser from 'body-parser';
 import { config } from './config';
-import { db } from './db';
+// import { db } from './db';
+
+import { router as userRouter } from './users'
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json());
+
+app.use('/', userRouter);
 
 
 app.listen(config.port, () => console.log('App is listening on url http://localhost:' + config.port));
